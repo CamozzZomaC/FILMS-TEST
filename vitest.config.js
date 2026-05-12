771b441a31d38e1948a800d16a332169fa006f6f
Carlos Amo <camoz@outlook.es>
2026-05-12 14:01:24 +0200

    config e2e

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,23 @@ export default defineConfig({
   test: {
     // ... Specify options here.
     globals: true,
-    setupFiles: './src/config/setup-test.ts'
-  },
-})
+    setupFiles: './src/config/setup-test.ts',
+    // opcionalmente
+        include: ['**/*.test.ts'],
+        // opcionalmente, para configurar la cobertura
+        coverage: {
+            include: ['src/**/*.ts'],
+            exclude: [
+                // Loader de la aplicación
+                'src/index.ts',
+                // Tipos y constantes
+                'src/**/entities/*.ts',
+                'src/**/types/*.ts',
+                'src/**/basic-errors.ts',
+                // DB Tools y rutas: test e2e
+                'src/config/db*.ts',
+                'src/**/router/*.ts'
+              ],
+            },
+        },
+});
